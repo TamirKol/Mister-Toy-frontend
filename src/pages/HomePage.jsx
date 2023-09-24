075e4d0ded5634d1f286ba783a2a936dc6a33731
TamirKol <tamirk9596@gmail.com>
2023-09-24 17:29:43 +0300

    home-page asset update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-
+import { utilService } from "../services/util.service.js"
 
 export function HomePage() {
 
     return (
         <section className="home-page">
             {/* <h2 >Welcome to MisterToy shop</h2 > */}
-                <img src="..//src//assets//img//home-page.jpg"/>
+                <img src={utilService.getAssetSrc("home-page.jpg")}/>
         </section >
     )
 }
